81169ab34ee3ee58e7e108d115123379b9ad33b9
Jannik Bartels <jannik1510@gmail.com>
2020-06-14 01:44:17 +0200

    refactor

--- PasswordHasher/Program.cs
+++ PasswordHasher/Program.cs
@@ -5,25 +5,18 @@ namespace ConsoleProgram
 {
     class Program
     {
-
         private static void Main(string[] args)
         {
-            Test();
+            Example();
         }
 
-        private static void Test()
+        private static void Example()
         {
-            var passwordSalt = PasswordService.GenerateSalt();
-            var passwordHash = PasswordService.ComputeHash("abc", passwordSalt);
-            var passwordHash2 = PasswordService.ComputeHash("abcd", passwordSalt);
+            byte[] passwordSalt = PasswordService.GenerateSalt();
+            byte[] passwordHash = PasswordService.ComputeHash("abcd", passwordSalt);
+            byte[] passwordHash2 = PasswordService.ComputeHash("abcd", passwordSalt);
 
-            if(PasswordService.Check(passwordHash, passwordHash2))
-                Console.WriteLine("true");
-            else
-                Console.WriteLine("false");
-
-
+            Console.WriteLine(PasswordService.Check(passwordHash, passwordHash2));
         }
-
     }
 }
